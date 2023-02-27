27afacf887ef30655c7e809a0497d40d9239a479
Mayank Gangwar <iammayankgangwarbly@gmail.com>
2023-02-27 11:23:21 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
-import Home from './home';
-import Movies from './movies';
-import Lists from './list';
-import Watch from './watch';
-  const Apps = () =>{
-return(
-  <>
-    <BrowserRouter>
-      <Routes>
+import Home from "./home";
+import Movies from "./movies";
+import Lists from "./list";
+import Watch from "./watch";
+const Apps = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
           <Route index element={<Home />} />
-          <Route path='/movies' element={<Movies />} />
-          <Route path='/lists' element={<Lists />} />
-          <Route path='/watch' element={<Watch />} />
-      </Routes>
-    </BrowserRouter>
-  </>
-);
-}
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/lists" element={<Lists />} />
+          <Route path="/watch" element={<Watch />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 export default Apps;
