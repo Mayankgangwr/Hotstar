0c4d5cd4e2c6a96a48e7f4616b69dca155595104
Mayank Gangwar <iammayankgangwarbly@gmail.com>
2023-02-27 15:41:55 +0000

    Update 6 files

--- src/nav.js
+++ src/nav.js
@@ -1,62 +1,71 @@
 import React from "react";
-
+import {Link} from 'react-router-dom';
+import "./nav.css";
 const Nav = () => {
+  const links = [
+    "bollywood",
+    "hollywood",
+    "south movies",
+    "dubbed movies",
+    "upcoming",
+    "popular",
+  ];
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-mdb-toggle="collapse"
-            data-mdb-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+      <nav class="navbar navbar-expand-lg navbar-dark bg-light-black">
+        <div class="container-fluid">
+          <a class="navbar-brand mt-2 mt-lg-0 d-mobile" href="#">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+              height="15"
+              alt="MDB Logo"
+              loading="lazy"
+            />
+          </a>
+          <div class="dropdown ms-auto d-mobile">
+            <a
+              class="text-reset me-2 dropdown-toggle hidden-arrow"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-mdb-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <i class="fas fa-bars"></i>
+            </a>
+            <ul
+              class="dropdown-menu dropdown-menu-end  bg-dark"
+              aria-labelledby="navbarDropdownMenuLink"
+            >
+              {links.map((el) => (
+                <li>
+                  <a class="dropdown-item text-capitalize text-light" href="#">
+                    {el}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div
+            class="collapse navbar-collapse d-desktop"
+            id="navbarSupportedContent"
           >
-            <i className="fas fa-bars"></i>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <a className="navbar-brand mt-2 mt-lg-0" href="#">
+            <a class="navbar-brand mt-2 mt-lg-0" href="#">
               <img
-                src="https://www.logo.wine/a/logo/Disney%2B/Disney%2B-Logo.wine.svg"
-                height="35"
+                src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+                height="15"
                 alt="MDB Logo"
                 loading="lazy"
               />
             </a>
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Bollywood
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Hollywood
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  South Movies
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Hindi Dubbed
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  UpComing
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Populer
-                </a>
-              </li>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              {links.map((el) => (
+                <li class="nav-item">
+                  <a class="nav-link text-capitalize" href="#">
+                    {el}
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
